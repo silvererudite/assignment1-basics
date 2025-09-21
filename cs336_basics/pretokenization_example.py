afd36453be27cc4293f8bba9900a2c5457dbfbaf
silvererudite <shamima.hossiain19@gmail.com>
2025-09-21 23:57:43 +0600

    Attempt to test tokenizer on validation data

--- cs336_basics/pretokenization_example.py
+++ cs336_basics/pretokenization_example.py
@@ -1,6 +1,6 @@
 import os
 from typing import BinaryIO
-
+from tokenization import BPE_TRAINING
 
 def find_chunk_boundaries(
     file: BinaryIO,
@@ -50,13 +50,26 @@ def find_chunk_boundaries(
 
 
 ## Usage
-with open(..., "rb") as f:
-    num_processes = 4
-    boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+if __name__ == "__main__":
 
-    # The following is a serial implementation, but you can parallelize this
-    # by sending each start/end pair to a set of processes.
-    for start, end in zip(boundaries[:-1], boundaries[1:]):
-        f.seek(start)
-        chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        # Run pre-tokenization on your chunk and store the counts for each pre-token
+    with open("../data/TinyStoriesV2-GPT4-valid.txt", "rb") as f:
+        num_processes = 4
+        vocab = {}
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        #print(boundaries)
+        # The following is a serial implementation, but you can parallelize this
+        # by sending each start/end pair to a set of processes.
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            chunk = chunk.split('<|endoftext|>')
+            #print(chunk)
+            chunk = ' '.join(chunk)
+            try:
+                new_vocab = BPE_TRAINING(chunk)
+                vocab.update(new_vocab)
+            except TypeError:
+                print("=========================================", chunk)
+                break
+        print("final vocab ", vocab)
+            # Run pre-tokenization on your chunk and store the counts for each pre-token`
--- cs336_basics/tokenization.py
+++ cs336_basics/tokenization.py
@@ -59,78 +59,91 @@ def get_key_with_max_value_lexicographically(d):
 
     # The first element of the sorted list will contain the desired key-value pair
     return sorted_items[0][0]
-corpus = """low low low low low lower lower widest widest widest newest newest newest newest newest newest"""
-
+# corpus = """low low low low low lower lower widest widest widest newest newest newest newest newest newest"""
+# corpus = """low low low low low
+# lower lower widest widest widest
+# newest newest newest newest newest newest
+# """
 # part 2.4
-PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-pre_tokenized_corpus = re.findall(PAT, corpus)
-# pre_tokenized_corpus = corpus.split(" ")
-print(pre_tokenized_corpus)
-#pretokens_corpus_with_space = [token.replace(" ", ".") for token in pre_tokenized_corpus if " " in token]
+def BPE_TRAINING(corpus):
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    pre_tokenized_corpus = re.findall(PAT, corpus)
+    # pre_tokenized_corpus = corpus.split(" ")
+    print(pre_tokenized_corpus)
+    #pretokens_corpus_with_space = [token.replace(" ", ".") for token in pre_tokenized_corpus if " " in token]
 
-pretokens_freq = dict(Counter(pre_tokenized_corpus))
-# initial vocabulary
-vocab = {bytes([i]): i for i in range(256)} 
-print("length of initial vocab ", len(vocab))
+    pretokens_freq = dict(Counter(pre_tokenized_corpus))
+    # initial vocabulary
+    vocab = {bytes([i]): i for i in range(256)} 
+    vocab['<|endoftext|>'] = 256
+    print("length of initial vocab ", len(vocab))
 
-num_merges = 5
+    num_merges = 6
 
-for merge in range(num_merges):
-    # print(vocab.keys())
-    print(f"=============== step {merge + 1} =============")
-    pretokens_freq = {tuple(token): freq for token, freq in pretokens_freq.items()}
-    print(f"pretokens in step {merge + 1} ", pretokens_freq)
+    for merge in range(num_merges):
+        # print(vocab.keys())
+        print(f"=============== step {merge + 1} =============")
+        pretokens_freq = {tuple(token): freq for token, freq in pretokens_freq.items()}
+        print(f"pretokens in step {merge + 1} ", pretokens_freq)
 
-    # pretokens_chars = [tuple(key) for key in pretokens_freq]
-    # print(pretokens_chars)
-    # this dict data structure is crucial to avoid multiple passes over the corpus, as we can now find 
-    # the pair/ bigram freq using the characters from the keys of the dict 
-    
-    bigram_freq = calculate_bigram_freq(pretokens_freq)
-    print(f"pretokens_bigram_freq in {merge + 1} step: ", bigram_freq)
-
-    # def get_max_pair(pretokens_bigram_freq):
-    #     return max(pretokens_bigram_freq, key=pretokens_bigram_freq.get)
-    
-
-    # print("max pair in the order inserted: ", get_max_pair(bigram_freq))
-    new_token = get_key_with_max_value_lexicographically(bigram_freq)
-    print(f"max pair lexicographically sorted in {merge + 1} step : ", new_token)
-
-    # def merge_step(vocab, new_token, pretokens_freq):
-    #     vocab[new_token.encode("utf-8")] = len(vocab) + 1
-    #     for word, freq in pretokens_freq.items():
-    #         for i in range(len(word)-1):
-    #             if len(word) > len(new_token) and (word[i] + word[i+1] == new_token):
-    #                 new_tuple =  word[:i] + (new_token,) + word[i+len(new_token):]
-    #                 pretokens_freq.pop(word)
-    #                 pretokens_freq[new_tuple] = freq
-    #     return pretokens_freq
-    def merge_step(vocab, new_token, pretokens_freq):
-        vocab[new_token.encode("utf-8")] = len(vocab) + 1
-        new_pretokens_freq = {}
+        # pretokens_chars = [tuple(key) for key in pretokens_freq]
+        # print(pretokens_chars)
+        # this dict data structure is crucial to avoid multiple passes over the corpus, as we can now find 
+        # the pair/ bigram freq using the characters from the keys of the dict 
         
-        for word, freq in pretokens_freq.items():
-            new_word = []
-            i = 0
-            while i < len(word):
-                if i < len(word)-1 and (word[i] + word[i+1]) == new_token:
-                    new_word.append(new_token)
-                    i += 2
-                else:
-                    new_word.append(word[i])
-                    i += 1
-            new_pretokens_freq[tuple(new_word)] = freq
+        bigram_freq = calculate_bigram_freq(pretokens_freq)
+        print(f"pretokens_bigram_freq in {merge + 1} step: ", bigram_freq)
 
-        return new_pretokens_freq
+        # def get_max_pair(pretokens_bigram_freq):
+        #     return max(pretokens_bigram_freq, key=pretokens_bigram_freq.get)
+        
 
+        # print("max pair in the order inserted: ", get_max_pair(bigram_freq))
+        new_token = get_key_with_max_value_lexicographically(bigram_freq)
+        print(f"max pair lexicographically sorted in {merge + 1} step : ", new_token)
 
-    pretokens_freq = merge_step(vocab, new_token, pretokens_freq)
-    print(f"length of vocab after merging in {merge+1} step ", len(vocab))
-    print(f"pretokens frequency after {merge + 1} step ", pretokens_freq)
-    # add the new merged token to the pretokens container
+        # def merge_step(vocab, new_token, pretokens_freq):
+        #     vocab[new_token.encode("utf-8")] = len(vocab) + 1
+        #     for word, freq in pretokens_freq.items():
+        #         for i in range(len(word)-1):
+        #             if len(word) > len(new_token) and (word[i] + word[i+1] == new_token):
+        #                 new_tuple =  word[:i] + (new_token,) + word[i+len(new_token):]
+        #                 pretokens_freq.pop(word)
+        #                 pretokens_freq[new_tuple] = freq
+        #     return pretokens_freq
+        def merge_step(vocab, new_token, pretokens_freq):
+            next_id = len(vocab)
+            if new_token not in vocab:
+                vocab[new_token] = next_id
+                next_id += 1
+            new_pretokens_freq = {}
+            
+            for word, freq in pretokens_freq.items():
+                new_word = []
+                i = 0
+                while i < len(word):
+                    if i < len(word)-1 and (word[i] + word[i+1]) == new_token:
+                        new_word.append(new_token)
+                        i += 2
+                    else:
+                        new_word.append(word[i])
+                        i += 1
+                new_pretokens_freq[tuple(new_word)] = freq
 
+            return new_pretokens_freq
 
+        
+        # add the new merged token to the pretokens container
+        pretokens_freq = merge_step(vocab, new_token, pretokens_freq)
+        print(f"length of vocab after merging in {merge+1} step ", len(vocab))
+        print(f"pretokens frequency after {merge + 1} step ", pretokens_freq)
+        print("=====================================")
+        if merge == 5:
+            for token, idx in vocab.items():
+                try:
+                    print(idx, token)
+                except UnicodeDecodeError:
+                    print(idx, repr(token))  # fallback for non-printable bytes
 
 
 
